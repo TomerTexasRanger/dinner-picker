722e5aaf455ce4ba051f79d986eda721662a5255
תומר ספיר <sapirtomer1989@gmail.com>
2021-05-31 11:48:56 +0300

    corrections

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,16 @@ const users = require("./routes/users");
 const path = require("path");
 
 app.use("/images", express.static(path.join(__dirname, "backend/images")));
-// app.use("/", express.static(path.join(__dirname, "angular")));
-app.use(cors());
+app.use("/", express.static(path.join(__dirname, "angular")));
+// app.use(cors());
+
 app.use(express.json());
 
 //routes
 app.use("/api/posts", posts);
 app.use("/api/users", users);
-// app.use((req, res, next) => {
-//   res.sendFile(path.join(__dirname, "angular", "index.html"));
-// });
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
